7a128f70711603175ea409a109ff4cca2076b164
efea <astarlioglu@outlook.com>
2023-03-21 18:50:17 +0200

    added 3_3

--- index.js
+++ index.js
@@ -38,10 +38,23 @@ app.get("/info", (request, response) => {
             `<p>Phone book has info for ${arrayInfo} people.</p>
              <p>${new Date()}</p>
             `
-        
     )
 }
 )
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+  
+    if(person){
+        response.json(person)
+    }
+    else
+    {
+        response.status(404).end()
+    }
+  })
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
